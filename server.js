3e85f811e1fd4485a978bfb0390d6d27c32716df
Alan Wang <alan098555@gmail.com>
2020-04-30 20:31:16 +1200

    MongoDB Connected

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+dotenv.config({ path: "./config/config.env" });
+
+const connectDB = require("./config/db");
+connectDB();
 
 const app = express();
 
 const transactionsRoute = require("./routes/transactionsRoute");
 app.use("/api/transactions", transactionsRoute);
 
-dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
