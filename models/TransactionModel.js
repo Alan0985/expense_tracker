20bf502bb493afec8ad7f9cb7fb88cfba6f7fa64
Alan Wang <alan098555@gmail.com>
2020-04-30 20:54:10 +1200

    Model Built

---
+++ models/TransactionModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    trim: true,
+    required: [true, "Please enter some text"],
+  },
+
+  amount: {
+    type: Number,
+    required: [true, "Please enter the amount"],
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("TransactionModel", TransactionSchema);
